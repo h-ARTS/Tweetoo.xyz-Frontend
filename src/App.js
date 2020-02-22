ff866c9305bd884ae8c6ce421760dbd3ca24db63
h-ARTS <hi5hanan@yahoo.com>
2020-02-22 20:38:22 +0100

    Preperation for token fetching
    - Axios
    - ErrorCatcher
    - Conditional for LocalStorage
    - Auth page for login/signup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import theme from './utils/theme';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import { Link, Router } from '@reach/router';
+import { MuiThemeProvider as ThemeProvider } from '@material-ui/core/styles';
+import { AuthPage } from './components/pages/AuthPage';
 
-function App() {
-  return (
-    <MuiThemeProvider theme={theme}>
-      <Button variant="contained" color="primary">
-        Hello World
-      </Button>
-      <Typography variant="h1"> Hello World </Typography>
-    </MuiThemeProvider>
-  );
+import ErrorCatcher from './ErrorCatcher';
+
+import jwtDecode from 'jwt-decode';
+import axios from 'axios';
+axios.defaults.baseURL = 'http://localhost:3000/api';
+
+const token = localStorage.token;
+if (token) {
+  const decoded = jwtDecode(token);
+  if (decoded.exp * 1000 < Date.now()) {
+    window.location.href = '/';
+  } else {
+    axios.defaults.headers['Authorization'] = token;
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
+        <nav>
+          <Link to="/home">Home</Link>
+          <Link to="dashboard">Dashboard</Link>
+        </nav>
+        <ErrorCatcher>
+          <Router>
+            <AuthPage path="/" />
+            <div path="/dashboard"></div>
+          </Router>
+        </ErrorCatcher>
+      </ThemeProvider>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/pages/AuthPage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Container } from '@material-ui/core';
+
+export class AuthPage extends Component {
+  render() {
+    return (
+      <Container>
+        <div>
+          <p>Here is Tweeto.xyz</p>
+        </div>
+      </Container>
+    );
+  }
+}
+
+export default AuthPage;
