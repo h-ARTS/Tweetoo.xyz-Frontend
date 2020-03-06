5cd8a66e242a3593ff6533b1e622b1bd26d42660
h-ARTS <hi5hanan@yahoo.com>
2020-03-06 23:22:45 +0100

    new demo data

--- src/common/ui/Timeline.js
+++ src/common/ui/Timeline.js
@@ -6,6 +6,8 @@ import { makeStyles, styled } from '@material-ui/core/styles';
 import { compose, spacing } from '@material-ui/system';
 // Components
 import Tweet from '../../app/home/Tweet';
+// Demo data
+import demoData from '../utils/demoData';
 
 const Box = styled('div')(compose(spacing));
 
@@ -20,52 +22,13 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-// Demo data
-const tweets = [
-  {
-    tweetId: '48598342568723',
-    fullText:
-      'This impressive paella is a perfect party dish and a fun meal #partydish',
-    fullName: 'Mr Paki',
-    handle: 'mr_paki',
-    retweetCount: 0,
-    likeCount: 0,
-    replies: [],
-    image: 'https://source.unsplash.com/random/470x240',
-    timestamp: '2020-02-24T20:18:29.000Z'
-  },
-  {
-    tweetId: '5463546456453',
-    fullText:
-      'Sunt ad proident esse consequat consectetur excepteur esse ut est esse ullamco sunt Lorem culpa. #TuesdayThoughts',
-    fullName: 'Henry Douglas',
-    handle: 'realHenreyDouglas',
-    retweetCount: 67,
-    likeCount: 89,
-    replies: [],
-    image: 'https://source.unsplash.com/random/450x220',
-    timestamp: '2020-02-24T20:18:29.000Z'
-  },
-  {
-    tweetId: '32454754654654',
-    fullText:
-      'Irure est voluptate dolor quis nostrud culpa consectetur quis in consectetur est sunt. Anim velit consequat non dolore quis pariatur magna non dolore sunt sint Lorem sunt. #TuesdayThoughts #Irure_est',
-    fullName: 'Super Nova',
-    handle: 'supernova1',
-    retweetCount: 12,
-    likeCount: 33,
-    replies: [],
-    timestamp: '2020-02-24T20:18:29.000Z'
-  }
-];
-
 export default function Timeline() {
   const classes = useStyles();
 
   return (
     <Box className={classes.timeline}>
       <Paper square>
-        {tweets.map(tweet => (
+        {demoData.map(tweet => (
           <Tweet tweet={tweet} key={tweet.tweetId} />
         ))}
       </Paper>
---
+++ src/common/utils/demoData.js
@@ -0,0 +1,48 @@
+export default [
+  {
+    tweetId: '48598342568723',
+    fullText:
+      'This impressive #stupid_lol paella is a perfect party dish and a fun meal #partydish',
+    fullName: 'Mr Paki',
+    handle: 'mr_paki',
+    retweetCount: 0,
+    likeCount: 0,
+    replies: [],
+    image: 'https://source.unsplash.com/random/470x240',
+    timestamp: '2020-02-24T20:18:29.000Z'
+  },
+  {
+    tweetId: '5463546456453',
+    fullText:
+      'Sunt ad proident esse consequat consectetur excepteur esse ut est esse ullamco sunt Lorem culpa. #TuesdayThoughts',
+    fullName: 'Henry Douglas',
+    handle: 'realHenreyDouglas',
+    retweetCount: 67,
+    likeCount: 89,
+    replies: [],
+    image: 'https://source.unsplash.com/random/450x220',
+    timestamp: '2020-02-24T20:18:29.000Z'
+  },
+  {
+    tweetId: '32454754654654',
+    fullText:
+      'Happy Sunday guys ❤️. Tell a man Shut up!😂🤯🎶 #Blaqbonez X #ikorodubois  ➖➖.#weplaytoomuch #Happinessisfree #yourfavouritemimickers🇳🇬 #ikorodutothe',
+    fullName: 'Super Nova',
+    handle: 'supernova1',
+    retweetCount: 12,
+    likeCount: 33,
+    replies: [],
+    timestamp: '2020-02-24T20:18:29.000Z'
+  },
+  {
+    tweetId: '32454754654654',
+    fullText:
+      'Irure est voluptate dolor quis nostrud culpa consectetur quis in consectetur est sunt. Anim velit consequat non dolore quis pariatur magna non dolore sunt sint Lorem sunt. #TuesdayThoughts #Irure_est',
+    fullName: 'Super Nova',
+    handle: 'supernova1',
+    retweetCount: 12,
+    likeCount: 33,
+    replies: [],
+    timestamp: '2020-02-24T20:18:29.000Z'
+  }
+];
