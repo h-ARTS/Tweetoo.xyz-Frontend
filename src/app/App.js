c5a6d321fe0efcdbf7f94d552cbc8b346deacb95
h-ARTS <hi5hanan@yahoo.com>
2020-04-06 17:22:43 +0200

    useEffect for token retreiving

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ThemeProvider } from '@material-ui/core/styles';
 import { Router, navigate } from '@reach/router';
 // JWT / Axios
 import axios from 'axios';
 import jwtDecode from 'jwt-decode';
 // Redux
-import store from '../redux/store';
+import { useSelector, useDispatch } from 'react-redux';
 import { logoutUser } from '../redux/actions/user.actions';
 import { fetchAllData } from '../redux/actions/data.actions';
+// Mui components
+import Backdrop from '@material-ui/core/Backdrop';
+import CircularProgress from '@material-ui/core/CircularProgress';
 // Component
 import ErrorCatcher from '../common/utils/ErrorCatcher';
 // Styling
@@ -19,28 +22,32 @@ import Home from './home/Home';
 import Profile from './profile/Profile';
 import Trending from './trending/Trending';
 import Layout from '../common/ui/Layout';
-import { Backdrop, CircularProgress } from '@material-ui/core';
-import { useSelector } from 'react-redux';
 
 axios.defaults.baseURL = 'http://localhost:6500';
 const token = localStorage.token;
 
-if (token) {
-  const decoded = jwtDecode(token);
-  if (decoded.exp * 1000 < Date.now()) {
-    store.dispatch(logoutUser());
-    navigate('/');
-  } else {
-    axios.defaults.headers.common['Authorization'] = token;
-    store.dispatch(fetchAllData());
-    navigate('/home');
-  }
-} else {
-  navigate('/');
-}
-
 export default function App() {
+  const dispatch = useDispatch();
   const { loading } = useSelector(state => state.ui);
+
+  useEffect(() => {
+    if (token) {
+      const decoded = jwtDecode(token);
+      if (decoded.exp * 1000 < Date.now()) {
+        dispatch(logoutUser());
+        navigate('/');
+      } else {
+        axios.defaults.headers.common['Authorization'] = token;
+        dispatch(fetchAllData());
+        const path =
+          window.location.pathname === '/' ? '/home' : window.location.pathname;
+        navigate(path);
+      }
+    } else {
+      navigate('/');
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <ThemeProvider theme={theme}>
