5e08163b1cdb61000718a2237ae30db68bdbeffb
h-ARTS <hi5hanan@yahoo.com>
2020-04-12 15:28:42 +0200

    delete tweet action creator and reducer

--- src/redux/actions/tweet.actions.js
+++ src/redux/actions/tweet.actions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { POST_TWEET, SET_AUTHENTICATED_USER } from '../types';
+import { POST_TWEET, SET_AUTHENTICATED_USER, DELETE_TWEET } from '../types';
 
 export const postTweet = data => async dispatch => {
   try {
@@ -12,6 +12,27 @@ export const postTweet = data => async dispatch => {
     dispatch({
       type: POST_TWEET,
       tweet: response.data.tweet
+    });
+    dispatch({
+      type: SET_AUTHENTICATED_USER,
+      user: response.data.user
+    });
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const deleteTweet = tweetId => async dispatch => {
+  try {
+    const response = await axios.delete(`/api/tweet/${tweetId}`);
+
+    if (!response) {
+      throw new Error(response);
+    }
+
+    dispatch({
+      type: DELETE_TWEET,
+      removed: response.data.removed
     });
     dispatch({
       type: SET_AUTHENTICATED_USER,
--- src/redux/reducers/tweet.reducers.js
+++ src/redux/reducers/tweet.reducers.js
@@ -1,4 +1,4 @@
-import { SET_TWEETS, POST_TWEET } from '../types';
+import { SET_TWEETS, POST_TWEET, DELETE_TWEET } from '../types';
 
 export default function(state = {}, action) {
   if (action.type === SET_TWEETS) {
@@ -7,6 +7,13 @@ export default function(state = {}, action) {
 
   if (action.type === POST_TWEET) {
     return [...state, action.tweet];
+  }
+
+  if (action.type === DELETE_TWEET) {
+    const removedFiltered = state.filter(
+      tweet => tweet._id !== action.removed._id
+    );
+    return [...removedFiltered];
   }
 
   return state;
