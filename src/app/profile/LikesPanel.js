ef737e04bf0bdbb2a8f20e808a0fd758bc3df71c
h-ARTS <hi5hanan@yahoo.com>
2020-04-12 11:08:55 +0200

    LikesPanel
    - Panels will be converted into generic panels to follow DRY

--- src/app/profile/LikesPanel.js
+++ src/app/profile/LikesPanel.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import ProfileTabPanel from './ProfileTabPanel';
 import Tweet from '../home/Tweet';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
 
-export default function LikesPanel({ value, index }) {
-  const { liked } = useSelector(state => state.currentUser);
-
-  return (
-    <ProfileTabPanel value={value} index={index}>
-      {liked.map(filteredTweet => (
-        <Tweet key={filteredTweet._id} tweet={filteredTweet} />
-      ))}
-    </ProfileTabPanel>
-  );
+export default function LikesPanel({ value, index, liked }) {
+  if (!liked) {
+    return (
+      <ProfileTabPanel value={value} index={index}>
+        <Box p={1}>
+          <Typography>
+            So far you have no liked tweets. Start discovering Tweetoo!
+          </Typography>
+        </Box>
+      </ProfileTabPanel>
+    );
+  } else {
+    return (
+      <ProfileTabPanel value={value} index={index}>
+        {liked.map(tweet => (
+          <Tweet key={tweet._id} tweet={tweet} />
+        ))}
+      </ProfileTabPanel>
+    );
+  }
 }
