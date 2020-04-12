9dfc5bb6ce6d21aa33b0cb613a16dc80ab4f14d7
h-ARTS <hi5hanan@yahoo.com>
2020-04-12 15:35:56 +0200

    Popover setup for deleting current user tweet

--- src/app/home/Tweet.js
+++ src/app/home/Tweet.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from '@reach/router';
+// Redux
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteTweet } from '../../redux/actions/tweet.actions';
 // Mui Components
 import Avatar from '@material-ui/core/Avatar';
 import Card from '@material-ui/core/Card';
@@ -8,8 +11,9 @@ import CardActionArea from '@material-ui/core/CardActionArea';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 // Mui Theme
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import { red } from '@material-ui/core/colors';
+import DeleteIcon from '@material-ui/icons/DeleteForeverTwoTone';
 // Components
 import TweetSubheader from './TweetSubheader';
 import MoreButton from './MoreButton';
@@ -17,6 +21,7 @@ import TweetAction from './TweetAction';
 import TweetTitle from './TweetTitle';
 import TweetText from './TweetText';
 import WithLinkTransformation from './WithLinkTransformation';
+import GenericPopover from '../../common/ui/GenericPopover';
 
 const useStyles = makeStyles(theme => ({
   timeline: {},
@@ -53,20 +58,43 @@ const TweetTextContainer = WithLinkTransformation(TweetText);
 
 export default function Tweet(props) {
   const classes = useStyles();
+  const theme = useTheme();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [listItems, setListItem] = useState([]);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const currentUser = useSelector(state => state.currentUser);
   const {
+    _id,
     handle,
     fullName,
     fullText,
     image,
-    tweetId,
     replies,
     retweet,
     retweetCount,
     likeCount,
-    createdAt
+    createdAt,
+    createdBy
   } = props.tweet;
   const replyCount = replies.length;
+
+  useEffect(() => {
+    if (currentUser._id === createdBy) {
+      setListItem([
+        ...listItems,
+        {
+          title: 'Delete Tweet',
+          divider: false,
+          callback: handleDeleteTweet,
+          icon: DeleteIcon,
+          iconColor: theme.palette.error.main,
+          textColor: 'error'
+        }
+      ]);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const handleNavigateToUser = () => {
     navigate(`/${handle}`);
@@ -74,57 +102,82 @@ export default function Tweet(props) {
 
   const navigateToTweet = event => {
     event.stopPropagation();
-    navigate(`/${handle}/tweet/${tweetId}`);
+    navigate(`/${handle}/tweet/${_id}`);
   };
 
   const handleStopPropagation = event => {
     event.stopPropagation();
   };
 
+  const handleMore = event => {
+    event.stopPropagation();
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = event => {
+    event.stopPropagation();
+    setAnchorEl(null);
+  };
+
+  const handleDeleteTweet = () => {
+    dispatch(deleteTweet(_id));
+  };
+
+  const popoverProps = {
+    id: Boolean(anchorEl) ? 'more-popover' : undefined,
+    open: Boolean(anchorEl),
+    anchorEl,
+    onClose: handleClose,
+    items: listItems
+  };
+
   return (
-    <Card className={classes.root} component="div" square varaint="outlined">
-      <CardActionArea
-        component="article"
-        role="article"
-        data-focusable="true"
-        tabIndex="0"
-        onClick={navigateToTweet}
-        className={classes.cardActionArea}
-        disableRipple
-      >
-        {retweet && <TweetSubheader handle={handle} />}
-        <CardHeader
-          className={classes.cardHeader}
-          avatar={
-            <Avatar aria-label="profile" className={classes.avatar}>
-              P
-            </Avatar>
-          }
-          title={
-            <TweetTitle
-              handle={handle}
-              fullName={fullName}
-              time={createdAt}
-              handleStopPropagation={handleStopPropagation}
-              onClick={handleNavigateToUser}
-            />
-          }
-          action={<MoreButton />}
-        />
-        <TweetTextContainer
-          className={classes.cardContent}
-          fullText={fullText}
-        />
-        {image && (
-          <CardMedia title="Random" image={image} className={classes.media} />
-        )}
-      </CardActionArea>
-      <CardActions className={classes.actions}>
-        <TweetAction actionType="reply" count={replyCount} />
-        <TweetAction actionType="retweet" count={retweetCount} />
-        <TweetAction actionType="like" count={likeCount} />
-        <TweetAction actionType="bookmark" />
-      </CardActions>
-    </Card>
+    <>
+      <Card className={classes.root} component="div" square varaint="outlined">
+        <CardActionArea
+          component="article"
+          role="article"
+          data-focusable="true"
+          tabIndex="0"
+          onClick={navigateToTweet}
+          className={classes.cardActionArea}
+          disableRipple
+        >
+          {retweet && <TweetSubheader handle={handle} />}
+          <CardHeader
+            className={classes.cardHeader}
+            avatar={
+              <Avatar aria-label="profile" className={classes.avatar}>
+                P
+              </Avatar>
+            }
+            title={
+              <TweetTitle
+                handle={handle}
+                fullName={fullName}
+                time={createdAt}
+                handleStopPropagation={handleStopPropagation}
+                onClick={handleNavigateToUser}
+              />
+            }
+            action={<MoreButton onClick={handleMore} />}
+          />
+          <TweetTextContainer
+            className={classes.cardContent}
+            fullText={fullText}
+          />
+          {image && (
+            <CardMedia title="Random" image={image} className={classes.media} />
+          )}
+        </CardActionArea>
+        <CardActions className={classes.actions}>
+          <TweetAction actionType="reply" count={replyCount} />
+          <TweetAction actionType="retweet" count={retweetCount} />
+          <TweetAction actionType="like" count={likeCount} />
+          <TweetAction actionType="bookmark" />
+        </CardActions>
+      </Card>
+      <GenericPopover {...popoverProps} />
+    </>
   );
 }
