7806761b1374b1c6d58b29076db4258b216a3c1f
h-ARTS <hi5hanan@yahoo.com>
2020-04-16 14:17:17 +0200

    current user state for NewTweetFormContainer

--- src/app/home/NewTweetFormContainer.js
+++ src/app/home/NewTweetFormContainer.js
@@ -1,22 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 // Mui Components
 import Paper from '@material-ui/core/Paper';
 // Mui Styles
 import { makeStyles } from '@material-ui/core/styles';
+// Components
 import NewTweetForm from './NewTweetForm';
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles({
   root: {
     borderWidth: '0 0 6px 0'
   }
-}));
+});
 export default function NewTweetFormContainer() {
   const classes = useStyles();
-  const user = { handle: 'mr_paki' };
+  const { current } = useSelector(state => state.user);
 
   return (
     <Paper className={classes.root} variant="outlined" square>
-      <NewTweetForm user={user} />
+      <NewTweetForm user={current} />
     </Paper>
   );
 }
