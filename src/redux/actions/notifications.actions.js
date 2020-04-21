ead729384b7e2a76af844cc96ed37ee4561269e2
h-ARTS <hi5hanan@yahoo.com>
2020-04-21 15:21:08 +0200

    markAllRead notifications action creator with type

--- src/redux/actions/notifications.actions.js
+++ src/redux/actions/notifications.actions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { SET_NOTIFICATIONS } from '../types';
+import { SET_NOTIFICATIONS, UPADTE_NOTIFICATIONS_READ } from '../types';
 
 export const getNotifications = () => async dispatch => {
   try {
@@ -12,6 +12,24 @@ export const getNotifications = () => async dispatch => {
     dispatch({
       type: SET_NOTIFICATIONS,
       notifications: response.data
+    });
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const markAllRead = () => async dispatch => {
+  const putBody = { read: true };
+  try {
+    const response = await axios.put('/api/notifications', putBody);
+
+    if (!response) {
+      throw new Error(response);
+    }
+
+    dispatch({
+      type: UPADTE_NOTIFICATIONS_READ,
+      updates: response.data
     });
   } catch (err) {
     throw err;
--- src/redux/reducers/notifications.reducers.js
+++ src/redux/reducers/notifications.reducers.js
@@ -1,9 +1,17 @@
 import initialState from '../initialState';
-import { SET_NOTIFICATIONS } from '../types';
+import { SET_NOTIFICATIONS, UPADTE_NOTIFICATIONS_READ } from '../types';
 
 export default function(state = initialState.notifications, action) {
   if (action.type === SET_NOTIFICATIONS) {
     return action.notifications;
+  }
+
+  if (action.type === UPADTE_NOTIFICATIONS_READ) {
+    const updated = state.map(notification => {
+      notification = { ...notification, ...action.updates };
+      return notification;
+    });
+    return updated;
   }
 
   return state;
