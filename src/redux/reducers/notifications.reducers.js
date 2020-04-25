c4af111dfac23620e821ef8ad41ee433ff645aa2
h-ARTS <hi5hanan@yahoo.com>
2020-04-25 14:37:04 +0200

    returning only updates on marking all read notifications

--- src/redux/reducers/notifications.reducers.js
+++ src/redux/reducers/notifications.reducers.js
@@ -7,11 +7,7 @@ export default function(state = initialState.notifications, action) {
   }
 
   if (action.type === UPADTE_NOTIFICATIONS_READ) {
-    const updated = state.map(notification => {
-      notification = { ...notification, ...action.updates };
-      return notification;
-    });
-    return updated;
+    return action.updates;
   }
 
   return state;
