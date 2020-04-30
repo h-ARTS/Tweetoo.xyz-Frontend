ea95c4d8790337e4ea29f43ef703932d7b14fd86
h-ARTS <hi5hanan@yahoo.com>
2020-04-30 16:47:13 +0200

    removed redundant code for height

--- src/common/ui/skeletons/SkeletonCoverImage.js
+++ src/common/ui/skeletons/SkeletonCoverImage.js
@@ -1,16 +1,9 @@
 import React from 'react';
 // Mui Components
 import Skeleton from '@material-ui/lab/Skeleton';
-import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles({
-  root: {
-    height: 200
-  }
-});
 const SkeletonCoverImage = () => {
-  const classes = useStyles();
-  return <Skeleton variant="rect" animation="wave" className={classes.root} />;
+  return <Skeleton variant="rect" animation="wave" height={200} />;
 };
 
 export default SkeletonCoverImage;
