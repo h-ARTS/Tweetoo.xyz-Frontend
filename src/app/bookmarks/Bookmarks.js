0e35684d811a34913d02ade255ec319bb9f916f4
h-ARTS <hi5hanan@yahoo.com>
2020-05-02 18:48:36 +0200

    SkeletonTweets for Bookmarks page on loading

--- src/app/bookmarks/Bookmarks.js
+++ src/app/bookmarks/Bookmarks.js
@@ -9,23 +9,25 @@ import Paper from '@material-ui/core/Paper';
 // Components
 import PageTitle from '../../common/ui/PageTitle';
 import RightBar from '../../common/ui/RightBar';
+import SkeletonTweet from '../../common/ui/skeletons/SkeletonTweet';
 import SuggestedFollowListContainer from '../trending/SuggestedFollowListContainer';
 import TrendsList from '../trending/TrendsList';
 import Tweet from '../home/Tweet';
 
 export default function Bookmarks() {
   const tweets = useSelector(state => state.tweets);
+  const { loading } = useSelector(state => state.ui);
 
   return (
     <>
       <Grid item xs={12} md={8}>
         <PageTitle renderTitle="Bookmarks" />
         <Paper variant="outlined">
-          {tweets
-            .filter(tweet => tweet.isBookmark)
-            .map(tweet => (
-              <Tweet tweet={tweet} key={tweet._id} />
-            ))}
+          {loading
+            ? [1, 2, 3].map(key => <SkeletonTweet key={key} />)
+            : tweets
+                .filter(tweet => tweet.isBookmark)
+                .map(tweet => <Tweet tweet={tweet} key={tweet._id} />)}
         </Paper>
       </Grid>
       <Hidden smDown>
