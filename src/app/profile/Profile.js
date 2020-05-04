4a507bbad91e5f0f0cc18cc9cd84e9b41c9bcbff
h-ARTS <hi5hanan@yahoo.com>
2020-05-04 17:23:46 +0200

    logout button removed from bottom

--- src/app/profile/Profile.js
+++ src/app/profile/Profile.js
@@ -11,17 +11,10 @@ import TweetPage from './TweetPage';
 import RightBar from '../../common/ui/RightBar';
 import TrendsList from '../discover/TrendsList';
 // MUI Components
-import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Hidden from '@material-ui/core/Hidden';
 
 export default function Profile() {
-  const store = useStore();
-
-  const handleLogout = () => {
-    store.dispatch(logoutUser());
-  };
-
   return (
     <>
       <Grid item xs={12} md={8}>
@@ -31,14 +24,6 @@ export default function Profile() {
           <FollowerPage path="followers" />
           <FollowerPage path="following" />
         </Router>
-        <Button
-          onClick={handleLogout}
-          variant="contained"
-          color="secondary"
-          disableElevation
-        >
-          Logout
-        </Button>
       </Grid>
       <Hidden smDown>
         <RightBar>
