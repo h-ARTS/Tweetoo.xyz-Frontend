b6da676cbc475e982a6b25a3cdac89d88b55c280
h-ARTS <hi5hanan@yahoo.com>
2020-05-14 10:43:34 +0200

    Get rid of the non accessible code

--- src/common/hooks/useDebounce.js
+++ src/common/hooks/useDebounce.js
@@ -18,38 +18,31 @@ export default function useDebounce(func, delay = 100) {
   // Keep track of the state
   const ref = useRef({ id: 0 });
 
-  // use latest function that's passed
+  // Use latest function that's passed
   ref.current.func = func;
 
+  // Make sure we are not calling this function too often!
   const debounce = useCallback(
     (...args) => {
-      ref.current.promise = new Promise((resolve, reject) => {
-        // Keep track of resolve and reject
-        ref.current.resolve = resolve;
-        ref.current.reject = reject;
-      });
-
       if (ref.current.timeout) {
         clearTimeout(ref.current.timeout);
       }
 
-      ref.current.timeout = setTimeout(async () => {
+      ref.current.timeout = setTimeout(() => {
         ref.current.timeout = undefined;
 
         const id = ref.current.id + 1;
         ref.current.id = id;
 
-        // Make a checkLatest function
+        // Check if it's the latest id
         const checkLatest = () => id === ref.current.id;
 
         if (checkLatest()) {
-          ref.current.resolve(ref.current.func(...args));
+          ref.current.func(...args);
         } else {
-          ref.current.reject('Function could not be executed');
+          console.error('Function in debounced could not be executed!');
         }
       }, delay);
-
-      return ref.current.promise;
     },
     [delay]
   );
