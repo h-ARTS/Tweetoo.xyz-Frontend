7d471a1b7957c5fca77d59248e4e77543c4658f3
h-ARTS <hi5hanan@yahoo.com>
2020-05-20 15:58:43 +0200

    upload tweet images action creator

--- src/redux/actions/tweet.actions.js
+++ src/redux/actions/tweet.actions.js
@@ -7,7 +7,9 @@ import {
   UNLIKE_TWEET,
   POST_RETWEET,
   DELETE_RETWEET,
-  UPDATE_TWEET_ENTRIES
+  UPDATE_TWEET_ENTRIES,
+  UPLOAD_TWEET_IMAGES_FOR_CACHE,
+  SAVE_UNIQUE_TWEET_IMAGE_IDS
 } from '../types';
 import isLikedPipe from '../../common/utils/isLikedPipe';
 import isRetweetPipe from '../../common/utils/isRetweetPipe';
@@ -28,51 +30,6 @@ export const postTweet = data => async dispatch => {
       type: SET_AUTHENTICATED_USER,
       user: response.data.user
     });
-  } catch (err) {
-    throw err;
-  }
-};
-
-export const deleteTweet = tweetId => async dispatch => {
-  try {
-    const response = await axios.delete(`/api/tweet/${tweetId}`);
-
-    if (!response) {
-      throw new Error(response);
-    }
-
-    dispatch({
-      type: DELETE_TWEET,
-      removed: response.data.removed
-    });
-    dispatch({
-      type: SET_AUTHENTICATED_USER,
-      user: response.data.user
-    });
-  } catch (err) {
-    throw err;
-  }
-};
-
-export const handleLikeTweet = (tweetId, type) => async dispatch => {
-  try {
-    const response = await axios.put(`/api/tweet/${tweetId}/${type}`);
-
-    if (!response) {
-      throw new Error(response);
-    }
-
-    if (type === 'like') {
-      dispatch({
-        type: LIKE_TWEET,
-        tweet: response.data.doc
-      });
-    } else {
-      dispatch({
-        type: UNLIKE_TWEET,
-        tweet: response.data.doc
-      });
-    }
   } catch (err) {
     throw err;
   }
@@ -112,12 +69,89 @@ export const postRetweet = (tweetId, retweet = true) => async dispatch => {
   }
 };
 
+export const handleLikeTweet = (tweetId, type) => async dispatch => {
+  try {
+    const response = await axios.put(`/api/tweet/${tweetId}/${type}`);
+
+    if (!response) {
+      throw new Error(response);
+    }
+
+    if (type === 'like') {
+      dispatch({
+        type: LIKE_TWEET,
+        tweet: response.data.doc
+      });
+    } else {
+      dispatch({
+        type: UNLIKE_TWEET,
+        tweet: response.data.doc
+      });
+    }
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const uploadTweetImagesForCache = files => async dispatch => {
+  const formData = new FormData();
+  for (let file of files) {
+    formData.append(file.name, file);
+  }
+  const config = {
+    headers: {
+      'content-type': `multipart/form-data; boundary=${formData._boundary}`
+    }
+  };
+  try {
+    if (formData.entries().length < 0)
+      throw Error('No Files in FormData object!');
+
+    const response = await axios.post(
+      '/api/tweet/cache-tweet-image',
+      formData,
+      config
+    );
+
+    if (!response) throw Error(response);
+
+    dispatch({
+      type: SAVE_UNIQUE_TWEET_IMAGE_IDS,
+      uniqueTweetImageIds: response.data.map(function getId(file) {
+        return file._id;
+      })
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+
 export const updateTweetEntries = (data, user) => async dispatch => {
-  console.log(data);
   let results = await isLikedPipe(data);
   results = isRetweetPipe(results, user);
   dispatch({
     type: UPDATE_TWEET_ENTRIES,
     entries: [...results]
   });
+};
+
+export const deleteTweet = tweetId => async dispatch => {
+  try {
+    const response = await axios.delete(`/api/tweet/${tweetId}`);
+
+    if (!response) {
+      throw new Error(response);
+    }
+
+    dispatch({
+      type: DELETE_TWEET,
+      removed: response.data.removed
+    });
+    dispatch({
+      type: SET_AUTHENTICATED_USER,
+      user: response.data.user
+    });
+  } catch (err) {
+    throw err;
+  }
 };
