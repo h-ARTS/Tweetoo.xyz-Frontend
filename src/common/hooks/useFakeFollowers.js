73b1c71c79a4ae8194806cad942424621814f133
h-ARTS <hi5hanan@yahoo.com>
2020-06-18 13:11:27 +0200

    Fix: Memory leak in fetching fake users during mount

--- src/common/hooks/useFakeFollowers.js
+++ src/common/hooks/useFakeFollowers.js
@@ -4,21 +4,32 @@ const useFakeFollowersApi = amount => {
   const [usersToFollow, setPotentialUserToFollow] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  const fetchFakeUsers = async () => {
-    const response = await fetch(
-      `https://randomuser.me/api/?results=${amount}`,
-      {
-        mode: 'cors'
-      }
-    );
+  // const fetchFakeUsers = async () => {
+  //   const response = await fetch(
+  //     `https://randomuser.me/api/?results=${amount}`,
+  //     {
+  //       mode: 'cors'
+  //     }
+  //   );
 
-    const potentialUsersToFollow = await response.json();
-    setPotentialUserToFollow(potentialUsersToFollow.results);
-    setLoading(false);
-  };
-
+  //   const potentialUsersToFollow = await response.json();
+  // };
+  
   useEffect(() => {
-    fetchFakeUsers();
+    let isActive = true;
+    
+    fetch(`https://randomuser.me/api/?results=${amount}`, {
+      mode: 'cors'
+    }).then(response => response.json()).then(data => {
+      if (isActive) {
+        setPotentialUserToFollow(data.results);
+        setLoading(false);
+      }
+    })
+
+    return () => {
+      isActive = false
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [loading]);
 
